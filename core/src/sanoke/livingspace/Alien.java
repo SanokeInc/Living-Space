ad454e3c54e88e6f811835715fcd4818edbd59fd
nlzz22 <nlzz_22@hotmail.com>
2015-12-27 21:40:47 +0800

    Replaced Alien Images

--- core/src/sanoke/livingspace/Alien.java
+++ core/src/sanoke/livingspace/Alien.java
@@ -1,6 +1,6 @@
 package sanoke.livingspace;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Alien {
@@ -9,12 +9,15 @@ public class Alien {
 	private float y;
 	private int type;
 	
+	private int currentFrameNumber;
+	private static final int FRAME_SWITCH_DELAY = 3;
+	
 	private float movementX;
 	private float movementY;
 	
 	private boolean onScreen;
 	
-	private Texture image;
+	private TextureRegion image;
 	
 	private int ALIEN_WIDTH;
 	private int ALIEN_HEIGHT;
@@ -30,13 +33,10 @@ public class Alien {
 	}
 	
 	private void initType(int alienType) {
-		switch (alienType) {
-			case 1:
-				image = Assets.alienType1;
-				ALIEN_WIDTH = 18;
-				ALIEN_HEIGHT = 21;
-				break;
-		}
+		currentFrameNumber = 0;
+		image = Assets.alienTypeFrame[alienType][currentFrameNumber];
+		ALIEN_WIDTH = Assets.ALIEN_WIDTH;
+		ALIEN_HEIGHT = Assets.ALIEN_HEIGHT;
 	}
 	
 	public Rectangle getAlienRegion() {
@@ -51,8 +51,12 @@ public class Alien {
 		return y;
 	}
 	
-	public Texture getImage() {
-		return image;
+	public TextureRegion getImage() {
+		TextureRegion imageToShow = image;
+		currentFrameNumber = (currentFrameNumber + 1) % (Assets.NUM_FRAMES_ALIEN * FRAME_SWITCH_DELAY); 
+		image = Assets.alienTypeFrame[type][currentFrameNumber / FRAME_SWITCH_DELAY];
+		
+		return imageToShow;
 	}
 	
 	public void move(float delta, int gameWidth, int gameHeight) {
--- core/src/sanoke/livingspace/Assets.java
+++ core/src/sanoke/livingspace/Assets.java
@@ -14,12 +14,15 @@ public class Assets {
     
     public static Texture warningSign;
 
-    public static Texture alienType1;
+    private static Texture [] alienTypeTemplate;
+    public static TextureRegion [][] alienTypeFrame; // [Alien Type][Alien Frame]
+    private static final int NUM_TYPE_ALIENS = 5;
     
-    private static TextureRegion alienType2;
-    private static TextureRegion alienType3;
-    public static TextureRegion[] alienTypes;
-
+    public static final int ALIEN_WIDTH = 36;
+    public static final int ALIEN_HEIGHT = 42;
+    public static final int NUM_FRAMES_ALIEN = 4;
+    private static final int FRAME_FACTOR = 2;
+    
     private static Music music;
     private static Sound missileSound;
 
@@ -28,15 +31,42 @@ public class Assets {
         spaceship = new Texture(Gdx.files.internal("ship_placeholder.png"));
         missile = new Texture(Gdx.files.internal("missile_placeholder.png"));
         warningSign = new Texture(Gdx.files.internal("warning_placeholder.png"));
-        alienType1 = new Texture(Gdx.files.internal("monster_placeholder.png"));
-   /*
         
+        loadAlienTemplates();
+        loadAlienFrames();
+   /*
         music = Gdx.audio.newMusic(Gdx.files.internal("*.mp3"));
         music.setLooping(true);
         music.setVolume(0.5f);
         
         missileSound = Gdx.audio.newSound(Gdx.files.internal("*.wav"));*/
     }
+
+	private static void loadAlienFrames() {
+		alienTypeFrame = new TextureRegion[NUM_TYPE_ALIENS][NUM_FRAMES_ALIEN];
+		
+		for (int i = 0; i < NUM_TYPE_ALIENS; i++) {
+        	alienTypeFrame[i] = new TextureRegion[NUM_FRAMES_ALIEN];
+        	
+        	for (int j = 0; j < NUM_FRAMES_ALIEN; j++) {
+            	int factorX = j % FRAME_FACTOR;
+            	int factorY = j / FRAME_FACTOR;
+            	
+            	alienTypeFrame[i][j] = new TextureRegion(alienTypeTemplate[i], ALIEN_WIDTH * factorX, ALIEN_HEIGHT * factorY,
+            			ALIEN_WIDTH, ALIEN_HEIGHT);
+            }
+        }
+	}
+
+	private static void loadAlienTemplates() {
+		alienTypeTemplate = new Texture[NUM_TYPE_ALIENS];
+		
+        alienTypeTemplate[0] = new Texture(Gdx.files.internal("Alien1_SpriteFrames.png"));
+        alienTypeTemplate[1] = new Texture(Gdx.files.internal("Alien2_SpriteFrames.png"));
+        alienTypeTemplate[2] = new Texture(Gdx.files.internal("Alien3_SpriteFrames.png"));
+        alienTypeTemplate[3] = new Texture(Gdx.files.internal("Alien4_SpriteFrames.png"));
+        alienTypeTemplate[4] = new Texture(Gdx.files.internal("Alien5_SpriteFrames.png"));
+	}
 
     public static void playMusic() {
         music.play();
